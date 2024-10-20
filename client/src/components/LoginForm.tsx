97c7212be72fd5d3e82905ec953f7b910e4e70c9
iamthesaint <stephenie2@me.com>
2024-10-20 17:10:46 -0400

    css

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { useState, type FormEvent, type ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import type { User } from '../models/User';
@@ -13,9 +13,12 @@ const LoginForm = ({ handleModalClose }: { handleModalClose: () => void }) => {
     password: '',
     savedBooks: [],
   });
-  const [login, { error, data }] = useMutation(LOGIN_USER);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const [login] = useMutation(LOGIN_USER);
+  const [validated, _] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
 
     setFormState({
@@ -26,6 +29,7 @@ const LoginForm = ({ handleModalClose }: { handleModalClose: () => void }) => {
 
   const handleFormSubmit = async (event: FormEvent) => {
     event.preventDefault();
+
   try {
       const { data } = await login({
         variables: {
@@ -48,53 +52,44 @@ const LoginForm = ({ handleModalClose }: { handleModalClose: () => void }) => {
   }
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email || ''}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password || ''}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
-
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    </main>
+      <>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+            Something went wrong with your login credentials!
+          </Alert>
+          <Form.Group className='mb-3'>
+            <Form.Label htmlFor='email'>Email</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Your email'
+              name='email'
+              onChange={handleInputChange}
+              value={formState.email || ''}
+              required
+            />
+            <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+          </Form.Group>
+  
+          <Form.Group className='mb-3'>
+            <Form.Label htmlFor='password'>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Your password'
+              name='password'
+              onChange={handleInputChange}
+              value={formState.password || ''}
+              required
+            />
+            <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+          </Form.Group>
+          <Button
+            disabled={!(formState.email && formState.password)}
+            type='submit'
+            variant='success'>
+            Submit
+          </Button>
+        </Form>
+      </>
   );
 };
 
